429efae5c006186c04c41823c2f4afac3f17aab0
slayne <majfeuguicheteau@gmail.com>
2025-03-24 17:10:54 +0100

    qlpskondq

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const $imageProfil = document.querySelector(".image-profil");
+$imageProfil.src = "./image/lune.png";
+
+const response = await fetch("https://api.github.com/users/tdimnet", {
+   const await = await.json
+  });
